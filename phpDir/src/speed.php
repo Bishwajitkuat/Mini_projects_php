c1abfc0e9019933ad40f73c5fe170d68c18bb47b
Bishwajitkuat <bishwajitkuat@gmail.com>
2023-03-31 22:33:16 +0300

    first working version for speed conversion and minimul css added

--- phpDir/src/speed.php
+++ phpDir/src/speed.php
@@ -4,12 +4,65 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="speed.css">
   <title>Speed conversion</title>
 </head>
 <body>
-  <div>
-    <form action="speed.php" method="POST" >
-      
+  <?php 
+  function speedConverter($speed, $from, $to) {
+    if ($to !== $from) {
+      if($to === "km/h") {
+        $speed = $speed *1.609;
+        return $speed;
+      } else {
+        $speed = $speed *0.621;
+        return $speed;
+      }
+    } else {
+      $speed = $speed;
+      return $speed;
+    }
+  }
+  ?>
+  <div class="speedDiv">
+    <form class="formDiv" action="speed.php" method="POST" >
+    <div class="speedInputDiv">
+      <label for="speed">Speed</label>
+      <input type="number" name="speed" id="speed">
+    </div>
+     <div class="speedUnitDiv">     
+        <label for="from">From</label>
+        <select name="from" id="from">
+        <option value="km/h">km/h</option>
+        <option value="m/h">m/h</option>
+        </select>
+        <label for="to">To</label>
+        <select name="to" id="to">
+        <option value="km/h">km/h</option>
+        <option value="m/h">m/h</option>
+        </select>
+      </div>
+      <button class="speedBtn" type="submit" >Convert</button>
+    </form>
+    <div class="speedResultDiv">
+    <?php 
+    if($_SERVER["REQUEST_METHOD"] === "POST") {
+    $hasData = $_POST["speed"] ? true : false;
+    if($hasData){
+      $speed = $_POST["speed"];
+      $fromUnit = $_POST["from"];
+      $toUnit = $_POST["to"];
+      $convertedSpeed = speedConverter($speed,$fromUnit, $toUnit);
+      echo "<h3>Result</h3>";
+      echo "<p class='resultPera'>".$speed.$fromUnit. " = " .$convertedSpeed.$toUnit."</p>";
+    } else {
+      echo "<p class='resultPera'>Please give some vale in speed field</p>";
+    }
+    }
+    ?>
+    </div>
+    <form action="index.php">
+     <button type="submit">Home</button> 
     </form>
   </div>
 </body>
