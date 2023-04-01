ec19c2f1d5d85713207e01c80d028ab54486ef86
BalThapa <info.balthapa@gmail.com>
2023-04-01 21:59:24 +0300

    temp

--- phpDir/src/temperature.php
+++ phpDir/src/temperature.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Temperature Conversion</title>
 </head>
 <body>
-    
+   <?php
+   function convertTemp ($tempValue, $tempType) {
+    if ($tempType == "fahrenheit"){
+        $conversion = ($tempValue * (9/5)) + (32);
+    }else {
+        
+    }
+   }
+   
+   
+   ?> 
 </body>
 </html>
