9870781dc5b531ed37b27c556e776237f37847b7
Bishwajitkuat <bishwajitkuat@gmail.com>
2023-04-14 20:20:57 +0300

    init.sql file added and docker-compose.yml file modified to create task table in bd and insert some data during docker compose

---
+++ init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE task(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  description VARCHAR(300) NOT NULL
+);
+
+INSERT INTO task (description) VALUES("Miniproject");
+INSERT INTO task (description) VALUES("Personal php assignment");
+INSERT INTO task (description) VALUES("UCL semifinal 2nd leg");
+INSERT INTO task (description) VALUES("React animal-bird project");
+
+
