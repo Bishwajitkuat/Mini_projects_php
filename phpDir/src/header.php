57d020ab9a5f39ff26380a80ae7a408522188a8a
Bishwajit Das <bishwajitkuat@gmail.com>
2023-04-20 18:59:29 +0300

    header section created

---
+++ phpDir/src/header.php
@@ -0,0 +1,22 @@
+
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="header.css">
+    <title>Mini projecs php</title>
+  </head>
+  <body>
+  <nav class="header__nav">
+  <h1>Mini projects</h1>
+  <ul>
+    <li> <a href="http://localhost:8001">Home</a> </li>
+    <li> <a href="http://localhost:8001/convertors/index.php">Conversion App</a> </li>
+    <li> <a href="http://localhost:8001/Contact-form/index.php">Contact Form</a> </li>
+    <li> <a href="http://localhost:8001/todo_app/index.php">To Dos</a> </li>
+  </ul>
+  </nav>
+  </body>
+  </html>
